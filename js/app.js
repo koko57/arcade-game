211b6736d7abe7085ef1fb223b336d1108618f22
Aggie Koko <agata.kosior57@gmail.com>
2018-04-18 09:37:11 +0200

    Enemy movement function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 // Enemies our player must avoid
-var Enemy = function () {
-    this.x = 0;
-    this.y = 0;
+var Enemy = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.width = 80;
+    this.height = 60;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     this.sprite = 'images/enemy-bug.png';
 };
 
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
+    this.x += this.speed*dt;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    
+    if (this.x > 500) {
+        return this.x = -100;
+    }
 
 };
 
@@ -29,15 +38,18 @@ Enemy.prototype.render = function () {
 var Player = function () {
     this.sprite = 'images/char-boy.png'
     this.x = 200;
-    this.y = 400;
+    this.y = 380;
 };
+
 
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 Player.prototype.update = function () {
 
 };
+
 Player.prototype.handleInput = function (evn) {
     switch (evn) {
         case 'left':
@@ -47,7 +59,7 @@ Player.prototype.handleInput = function (evn) {
             break;
         case 'up':
             if (this.y > 0) {
-                this.y -= 100;
+                this.y -= 80;
             }
             break;
         case 'right':
@@ -57,12 +69,12 @@ Player.prototype.handleInput = function (evn) {
             break;
         case 'down':
             if (this.y < 400) {
-                this.y += 100;
+                this.y += 80;
             }
             break;
     }
-
 };
+
 
 
 // Now instantiate your objects.
@@ -71,10 +83,11 @@ Player.prototype.handleInput = function (evn) {
 var allEnemies = [];
 var player = new Player;
 
-var enemy = new Enemy;
-var enemy2 = new Enemy;
+var enemy = new Enemy(-60, 60, 40);
+var enemy2 = new Enemy(-80, 145, 50);
+var enemy3 = new Enemy(-100, 230, 70);
 
-allEnemies.push(enemy);
+allEnemies.push(enemy, enemy2, enemy3);
 
 document.addEventListener('keyup', function (e) {
     var allowedKeys = {
